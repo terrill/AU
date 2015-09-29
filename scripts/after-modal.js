58a823ceacbfc3415a752d46b12b17d0c6525067
Terrill Thompson <terrill@terrillthompson.com>
2015-09-29 09:50:32 -0700

    Add placeholder for accessible modal

--- scripts/after-modal.js
+++ scripts/after-modal.js
@@ -7,6 +7,41 @@
   
 $(document).ready(function() { 
 
-  // code here 
+  var spanText = 'See also our '; 
+  var spanLink = $('<a>')
+    .text('cheat sheet of accessibility problems')
+    .on('click',function(event) { 
+      event.stopPropagation();
+      showModal();
+    });
+  $('#cheatSheetLinkSpan').text(spanText).append(spanLink).append('.');
+  
+  // handle click on X or Ok button in modal dialog 
+  $(document).on('click','#modalXButton, #modalOkButton',function(event) { 
+    $('#modalContent').hide();
+    $('#modalMask').hide();      
+  })
+  
+  function showModal() { 
+    
+    var winWidth = $(window).width();
+    var winHeight = $(window).height();
+    var modalSize = 400; 
+    var modalLeft = ((winWidth-modalSize)/2) + 'px';
+    var modalTop = ((winHeight-modalSize)/2) + 'px';
+	
+    $('#modalContent')
+      .css({ 
+        'top': modalTop,
+        'left': modalLeft      
+      })
+      .show();
+
+    $('#modalMask').show(); 
+    
+    // place focus on the close button at the top of the modal 
+    $('#modalXButton').focus(); 
+  }
+ 
     
 });
